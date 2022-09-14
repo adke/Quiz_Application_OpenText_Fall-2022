e33915dd154a2a356b329b56c134ae2e6ee226ea
JackBai2000 <jackybai2000@hotmail.com>
2022-09-14 14:14:16 -0400

    Classes and File initialization
    Created base classes and files to begin backend project

---
+++ quiz-app/src/main/java/com/example/quizapp/answers/Answers.java
@@ -0,0 +1,7 @@
+package com.example.quizapp.answers;
+
+public class Answers {
+    private Long quizID;
+    private Long answerID;
+    private String content;
+}
---
+++ quiz-app/src/main/java/com/example/quizapp/quizzes/QuizService.java
@@ -0,0 +1,4 @@
+package com.example.quizapp.quizzes;
+
+public class QuizService {
+}
