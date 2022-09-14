b662495e29881877b90783538e0f40c972f4b1b2
Eric Wang <weric@opentext.com>
2022-09-14 16:13:27 -0400

    Created function outlines for QuizController and QuizService

--- quiz-app/src/main/java/com/example/quizapp/quizzes/QuizService.java
+++ quiz-app/src/main/java/com/example/quizapp/quizzes/QuizService.java
@@ -1,4 +1,27 @@
 package com.example.quizapp.quizzes;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public class QuizService {
+    public List<Quiz> getQuizzes() {
+        return null;
+    }
+
+    public Quiz getQuiz(String id) {
+        return null;
+    }
+
+    public ResponseEntity<String> newQuiz() {
+        return null;
+    }
+
+    public ResponseEntity<String> editQuiz(String id) {
+        return null;
+    }
+
+    public ResponseEntity<String> deleteQuiz(String id) {
+        return null;
+    }
 }
