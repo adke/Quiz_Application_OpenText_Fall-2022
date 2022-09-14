222421725f97bebb99035442c4bc22707cd10b62
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2022-09-14 11:58:20 -0400

    Takequiz + Addquiz pages

---
+++ frontend_nextjs/pages/addquiz.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Nav from '../components/Nav'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+
+export default function Addquiz() {
+  return (
+    <div>
+      <Head>
+        <title>Quizzler</title>
+      </Head>
+      <Nav />    
+      
+    </div>
+  )
+}
---
+++ frontend_nextjs/pages/takequiz.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Nav from '../components/Nav'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+
+export default function Takequiz() {
+  return (
+    <div>
+      <Head>
+        <title>Quizzler</title>
+      </Head>
+      <Nav />    
+      
+    </div>
+  )
+}
