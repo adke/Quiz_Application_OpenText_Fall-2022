1fd5750ca3b37b246a48cb67fb3026fcac67bd43
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2022-09-14 11:54:39 -0400

    Frontend-setup

---
+++ frontend_nextjs/components/MainSection.js
@@ -0,0 +1,11 @@
+
+
+function MainSection(){
+    return(
+        <div className="standard-format">
+            <button className="button-17">Let's Make a Quiz!</button>
+        </div>
+    )
+}
+
+export default MainSection;
--- frontend_nextjs/pages/index.js
+++ frontend_nextjs/pages/index.js
@@ -1,15 +1,21 @@
 import Head from 'next/head'
+import Nav from '../components/Nav'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+import MainSection from '../components/MainSection'
+import QuizCard from '../components/QuizCard'
 
 export default function Home() {
   return (
-    // Everything has to be within this PARENT div
     <div>
       <Head>
-        <title>Quizzer</title>
-        <meta name='keywords' content='web development, fullstack, nextjs, springboot' />
+        <title>Quizzler</title>
       </Head>
-
-      <h1>Quizzer App</h1>
+      
+      <Nav />    
+      <MainSection />
+      <h2>Quizzes</h2>
+      <QuizCard />
     </div>
   )
 }
