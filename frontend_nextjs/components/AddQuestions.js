dbf2ab0759b2f83ec0cf468e909549b002ef0727
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2022-09-16 16:15:53 -0400

    Update AddQuizPage

--- frontend_nextjs/components/AddQuestions.js
+++ frontend_nextjs/components/AddQuestions.js
@@ -1,38 +1,53 @@
 import questionStyles from '../styles/AddQ.module.css'
-
+import styles from '../styles/Card.module.css'
+import { useState } from 'react'
 
 function AddQuestions(){
+
     // add questions function
     const addFields = () => {
         console.log('test');
 
         var number = document.getElementById("questions").value;
         var form = document.getElementById("form");
-        var ul = document.getElementById("ul");
-        var div = document.getElementById("div");
 
         while (form.hasChildNodes()) {
             form.removeChild(form.lastChild);
         }
         for (let i = 0; i < number; i++){
-            form.appendChild(document.createTextNode("Question " + (i+1) + ":    "));
+            // ul Element
+            var ul = document.createElement("ul");
+            ul.id = "ul"
+            form.appendChild(ul);
+
+            ul.appendChild(document.createTextNode("Question " + (i+1) + ":    "));
+            // Question Input
             var input = document.createElement("input");
             input.type = "text";
+            input.className = "Qinput";
+            ul.appendChild(document.createElement("br"));
+            ul.appendChild(input);                            // Question input
+            
+            ul.appendChild(document.createElement("br"));
 
-            form.appendChild(document.createElement("br"));
-            form.appendChild(input);
-            form.appendChild(document.createElement("br"));
-
-            form.appendChild(document.createTextNode("Answer:    "));
+            ul.appendChild(document.createTextNode("Answer:    "));
             for (let j = 0; j < 4; j++) {
                 var input = document.createElement("input");
                 input.type = "text";
-                form.appendChild(document.createElement("br"));
-                form.appendChild(input);
-                console.log('hi');
+
+                if (j == 0) {
+                    input.className = "A_right_input";
+                    console.log('testing');
+                } else {
+                    
+                    input.className = "A_wrong_input";
+                }
+                
+                ul.appendChild(document.createElement("br"));
+                ul.appendChild(input);                          // Answer input
             }
 
-            form.appendChild(document.createElement("br"));
+            ul.appendChild(document.createElement("br"));
             
         }
 
@@ -41,17 +56,26 @@ function AddQuestions(){
 
     return(
         <div className={questionStyles.addq}>
+            
+            <div>
+                <label>Instructions:</label>
+                <p>Input the number of questions below and click "Add Questions". Then input the correct answer in the green box and the incorrect answers in the red box.</p>
+            </div>
+            
             <label>Number of Questions:</label>
             <input className={questionStyles.noq} type='text' id='questions' name='questions' />
             
-            <form id="form" required>
-                {/* <div id="div" required/> */}
+            <form id='form' action='/log.json' method='post'>
+
             </form>
 
-            <br />
-            <button href='#' id='addButton' className="btn btn-outline-primary" onClick={addFields}>Add Questions</button>
-            <span />
-            <button type='submit' value='Submit' id='subButton' className="btn btn-outline-primary" >Submit</button>
+            <div className={questionStyles.buttons}>
+                <br />
+                <button href='#' id='addButton' className="btn btn-outline-primary" onClick={addFields}>Add Questions</button>
+                <span />
+                <button type='submit' className="btn btn-outline-primary">Submit</button>
+                {/* <input type='submit' className="btn btn-outline-primary" /> */}
+            </div>
         </div>
     )
 }
