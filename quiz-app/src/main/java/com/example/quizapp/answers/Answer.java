d7a962c29f16af2a8fc574c55c3bb25b669a891d
brianwu33 <brianwu20020303@gmail.com>
2022-09-16 14:21:00 -0400

    Added more annotations in Answer.java

--- quiz-app/src/main/java/com/example/quizapp/answers/Answer.java
+++ quiz-app/src/main/java/com/example/quizapp/answers/Answer.java
@@ -8,13 +8,30 @@ import javax.persistence.*;
 @Data
 @Entity
 public class Answer {
+    //@Id Sets answerID to be the primary key of the table
     @Id
-    @GeneratedValue
+    //@SequenceGenerator and @GeneratedValue is use for generate a big serial integer for id
+    @SequenceGenerator(name = "answer_sequence", sequenceName = "answer_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "answer_sequence")
+    //@Column specifies properties for the column
+    //name specifies the name of the field in the database
+    //updatable specifies if this field can be updated
+    @Column(
+            name = "answer_id",
+            updatable = false
+    )
     private Long answerID;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "questionID")
     private Question question;
 
+    //@nullable sets if this field can be null
+    //TEXT
+    @Column(
+            name = "content",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String content;
 }
--- quiz-app/src/main/java/com/example/quizapp/questions/Question.java
+++ quiz-app/src/main/java/com/example/quizapp/questions/Question.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Data
-@Entity
+@Entity(name = "Question")
 public class Question {
     @Id
     @GeneratedValue
