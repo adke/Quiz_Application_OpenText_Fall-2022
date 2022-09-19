ed178aeb763c5592fd6f0e3d4007665b5606b862
Adish Shah <adish.shah2003@gmail.com>
2022-09-19 11:12:55 -0400

    Design has changed and added error 404 feature

--- frontend_nextjs/components/Nav.js
+++ frontend_nextjs/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import navStyle from '../styles/Nav.module.css'
 
+<<<<<<< Updated upstream
 function Nav(){
     return(
         <header className={navStyle.nav}>
@@ -10,6 +11,23 @@ function Nav(){
                 </ul>
             </Link>
         </header>
+=======
+
+function Nav() {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Quizzer</h1>
+            </div>
+            <div className="anchor">
+                <Link href="/"><a id="home">Home</a></Link>
+                <Link href="/about"><a id="about">About</a></Link>
+            </div>
+
+
+
+        </nav>
+>>>>>>> Stashed changes
     )
 }
 
--- frontend_nextjs/components/QuizCard.js
+++ frontend_nextjs/components/QuizCard.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Card.module.css'
 
+<<<<<<< Updated upstream
 function QuizCard({}){
     return(
             <div className='card-format'>
@@ -20,6 +21,33 @@ function QuizCard({}){
                         <h4><b>Quiz XXX3</b></h4> 
                         <p>2022-09-16</p> 
                     </div>
+=======
+function QuizCard({ }) {
+    return (
+        <div className='cardformat'>
+            <div className={styles.card}>
+                <div className={styles.container}>
+                    <h4><b>Quiz XXX1</b></h4>
+                    <p>Subject</p>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <div className={styles.container}>
+                    <h4><b>Quiz XXX2</b></h4>
+                    <p>Subject</p>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <div className={styles.container}>
+                    <h4><b>Quiz XXX3</b></h4>
+                    <p>Subject</p>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <div className={styles.container}>
+                    <h4><b>Quiz XXX4</b></h4>
+                    <p>Subject</p>
+>>>>>>> Stashed changes
                 </div>
             </div>
     );
