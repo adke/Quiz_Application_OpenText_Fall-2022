c6731cc0c67bd8e6631088b0ddcf9eff1e813a4f
JackBai2000 <jackybai2000@hotmail.com>
2022-09-21 10:48:58 -0400

    Created Quiz Test Service File
    Created starter file for tests and added Junit Jupiter dependency

---
+++ quiz-app/src/test/java/com/example/quizapp/service/QuizServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.quizapp.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizServiceTest {
+
+    @Test
+    @DisplayName("Should create new Quiz")
+    void createNewQuiz() {
+    }
+
+    @Test
+    @DisplayName("Should get all the Quizzes")
+    void getAllQuiz() {
+    }
+
+    @Test
+    @DisplayName("Should get a quiz based on ID")
+    void getQuiz() {
+    }
+
+    @Test
+    @DisplayName("Should delete a quiz based on ID")
+    void deleteQuiz() {
+    }
+}
